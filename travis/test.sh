ed111ea722f5d0f4d6d76c3d37320b749e63a559
Beri Shifaw <bshifaw@broadinstitute.com>
2018-06-26 15:19:58 -0400

    add gcs auth

--- travis/test.sh
+++ travis/test.sh
@@ -5,4 +5,4 @@
 #downloand cromwell
 wget http://github.com/broadinstitute/cromwell/releases/download/32/cromwell-32.jar
 #excute java script
-java -Dconfig.file=/travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/ -jar cromwell-32.jar run processing-for-variant-discovery-gatk4.wdl -i processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json
+java -Dconfig.file=travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/ -jar cromwell-32.jar run processing-for-variant-discovery-gatk4.wdl -i processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json
