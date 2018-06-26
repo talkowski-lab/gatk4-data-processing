5fa623ca8bf60b0224f4272f1f5b8c2e05c88c05
Beri Shifaw <bshifaw@broadinstitute.com>
2018-06-26 15:47:01 -0400

    edit the bucket location

--- travis/test.sh
+++ travis/test.sh
@@ -1,8 +1,7 @@
 
 #!/bin/bash
-#declare wdls to execute
 
 #downloand cromwell
-wget http://github.com/broadinstitute/cromwell/releases/download/32/cromwell-32.jar
-#excute java script
-java -Dconfig.file=travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/ -jar cromwell-32.jar run processing-for-variant-discovery-gatk4.wdl -i processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json
+wget http://github.com/broadinstitute/cromwell/releases/download/33/cromwell-33.jar
+#excute wdl script
+java -Dconfig.file=travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-32.jar run processing-for-variant-discovery-gatk4.wdl -i processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json
