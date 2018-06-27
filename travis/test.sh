db73d84b880f8330756fb860b8d71fa5c958f01a
Beri Shifaw <bshifaw@broadinstitute.com>
2018-06-27 16:20:12 -0400

    minor correction

--- travis/test.sh
+++ travis/test.sh
@@ -5,6 +5,6 @@ wdl="processing-for-variant-discovery-gatk4.wdl"
 json="processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json"
 
 #downloand cromwell
-wget http://github.com/broadinstitute/cromwell/releases/download/$cromwell_version/cromwell-$cromwell_version.jar
+wget http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
-java -Dconfig.file=travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-$cromwell_version.jar run $wdl -i $json
+java -Dconfig.file=travis/resources/google-adc.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}
