cc5d6a9721ae701aff59215e2c20bda81c20cbd8
Beri Shifaw <bshifaw@broadinstitute.com>
2018-06-29 15:03:34 -0400

    added wait time for test

--- travis/test.sh
+++ travis/test.sh
@@ -3,13 +3,16 @@
 cromwell_version="33"
 wdl="processing-for-variant-discovery-gatk4.wdl"
 json="processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json"
+test_complete=false
 
 # Sleep one minute between printouts, but don't zombie for more than two hours. This prevents Travis from automatically exiting due to lack of output while workflow is running.
 for ((i=0; i < 180; i++)); do
     sleep 60
-    printf "…"
+    printf "…"${i}
 done &
 #downloand cromwell
 wget http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
 java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}
+#indicate test is complete
+test_complete=true
