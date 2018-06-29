aa8543b9e36cab89b812b353e7beb741dd097c7e
Beri Shifaw <bshifaw@broadinstitute.com>
2018-06-29 11:34:24 -0400

    moved travis wait to test.sh

--- travis/test.sh
+++ travis/test.sh
@@ -4,7 +4,9 @@ cromwell_version="33"
 wdl="processing-for-variant-discovery-gatk4.wdl"
 json="processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json"
 
+#heartbeat
+
 #downloand cromwell
 wget http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
-java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}
+travis_wait 60 java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}
