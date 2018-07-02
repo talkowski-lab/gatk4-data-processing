81e0a9289f2990c2d513365a7578cb9c06285fc1
Beri Shifaw <bshifaw@broadinstitute.com>
2018-07-02 13:10:01 -0400

    removed time extending script and made input smaller

--- travis/test.sh
+++ travis/test.sh
@@ -6,22 +6,18 @@ json="processing-for-variant-discovery-gatk4.hg38.wgs.inputs.json"
 
 
 # Sleep one minute between printouts, but don't zombie for more than two hours. This prevents Travis from automatically exiting due to lack of output while workflow is running.
-#for ((i=0; i < 180; i++)); do
-#    sleep 60
-#    printf "…"${i}
-#done &
+for ((i=0; i < 180; i++)); do
+    sleep 60
+    printf "…"${i}
+done &
 #downloand cromwell
 wget http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
-#java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}; test_complete=true &
-#indicate test is complete
-#test_complete=true
+java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json};
 
-#whats in the workflow-log
-#tail -f cromwell-workflow-logs/workflow* &
 
-if java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json} > output.txt 2>&1 ; then
-    touch succes.txt;
-else
-    touch failed.txt;
-fi &
+#if java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json} > output.txt 2>&1 ; then
+#    touch succes.txt;
+#else
+#    touch failed.txt;
+#fi &
