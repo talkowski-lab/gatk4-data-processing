c2f9de4256cbdff6b3e504f3b3d7566784aa8758
Beri Shifaw <bshifaw@broadinstitute.com>
2018-07-02 15:29:32 -0400

    correction wget

--- travis/test.sh
+++ travis/test.sh
@@ -11,6 +11,6 @@ for ((i=0; i < 180; i++)); do
     printf "…"${i}
 done &
 #downloand cromwell
-wget -O http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
+wget -o http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
 java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json};
