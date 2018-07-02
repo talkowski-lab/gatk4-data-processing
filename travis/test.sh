21bc736f2563779b00660fb44f06aec436919ff0
Beri Shifaw <bshifaw@broadinstitute.com>
2018-07-02 09:19:03 -0400

    Added if else statment to java command

--- travis/test.sh
+++ travis/test.sh
@@ -13,9 +13,15 @@ done &
 #downloand cromwell
 wget http://github.com/broadinstitute/cromwell/releases/download/${cromwell_version}/cromwell-${cromwell_version}.jar
 #excute wdl script
-java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}; test_complete=true &
+#java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}; test_complete=true &
 #indicate test is complete
 #test_complete=true
 
 #whats in the workflow-log
 #tail -f cromwell-workflow-logs/workflow* &
+
+if java -Dconfig.file=travis/resources/google-sac.conf -Dbackend.providers.JES.config.project=broad-dsde-outreach -Dbackend.providers.JES.config.root=gs://beri-test/gatk-workflows-travis-tests/ -jar cromwell-${cromwell_version}.jar run ${wdl} -i ${json}; then
+    test_complete=true
+else
+    test_complete=false
+fi &
